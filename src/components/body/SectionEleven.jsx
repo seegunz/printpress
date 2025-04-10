86196cf396574360e8f1f6435e81fd2937114e08
whatyouthinkz <personalniga@gmail.com>
2025-04-10 08:42:02 -0500

    remaining the footer

--- src/components/body/SectionEleven.jsx
+++ src/components/body/SectionEleven.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SectionEleven() {
   return (
-    <div className='flex flex-col md:flex-row justify-between 2xl:justify-around md:items-center mx-2 md:mx-20 2xl:mx-40 mt-10 bg-sky-700 text-white px-10 gap-y-10 2xl:px-40 py-24 rounded-2xl'>
+    <div className='flex flex-col md:flex-row justify-between 2xl:justify-around md:items-center mx-2 md:mx-20 2xl:mx-40 mt-10 bg-sky-700 text-white px-10 gap-y-10 2xl:px-40 py-24 rounded-4xl'>
         <section className='xl:w-1/2'>
             <h1 className='text-2xl lg:text-4xl font-semibold'>ready to level up your income?</h1>
             <h1 className='text-3xl lg:text-5xl font-bold'>become a reseller.</h1>
--- src/components/body/SectionTwelve.jsx
+++ src/components/body/SectionTwelve.jsx
@@ -3,10 +3,10 @@ import product from '/imgs/product.png'
 
 export default function SectionTwelve() {
   return (
-    <div className='flex justify-between items-center px-40 mt-10 bg-gray-100 px-8 py-2 rounded-2xl'>
-        <section className='w-1/2'>
+    <div className='flex justify-between items-center px-2 py-4 lg:py-0 md:px-20 xl:px-40 mt-10 bg-gray-100 px-8 rounded-2xl'>
+        <section className='lg:w-1/2'>
             <h1 className='text-2xl font-bold'>Need help? We're here for you</h1>
-            <p className=' mt-5'>Our customer service are readily available to respond to your request</p>
+            <p className=' mt-5 text-gray-600'>Our customer service are readily available to respond to your request</p>
             <div className='flex gap-4 mt-5 text-[14px] text-sky-700'>
               <a href="#">Chat with Us</a>
               <a href="#">Help Centre</a>
@@ -14,8 +14,8 @@ export default function SectionTwelve() {
               <a href="#">Whatsapp</a>
             </div>
         </section>
-        <section>
-            <img src={product} alt="" width={200}/>
+        <section className='hidden lg:flex'>
+            <img src={product} alt=""  />
         </section>
     </div>
   )
